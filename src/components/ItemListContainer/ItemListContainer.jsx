0df5957c607447323b4bc436b16fcef0023c053c
Juan Ignacio Torielli Rios <juan3229988@gmail.com>
2023-02-01 21:09:37 -0300

    Detalles finales

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./ItemListContainer.css";
 
-const ItemListContainer = () => {
-  return <div></div>;
+const ItemListContainer = (prop) => {
+  const greetingStyle = {
+    fontSize: 100,
+    marginTop: "10rem",
+    color: "green",
+  };
+  return <h2 style={greetingStyle}>{prop.greeting}</h2>;
 };
 
 export default ItemListContainer;
